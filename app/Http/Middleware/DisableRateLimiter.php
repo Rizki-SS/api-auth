531afb952075ab4e32e440cc582e4df76ff0db9a
isyak rizqi <isyak@jagoanhosting.com>
2023-06-27 09:12:57 +0000

    add middleware disable limiter

---
+++ app/Http/Middleware/DisableRateLimiter.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Cache\RateLimiter;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DisableRateLimiter
+{
+    /**
+     * The rate limiter instance.
+     *
+     * @var \Illuminate\Cache\RateLimiter
+     */
+    protected $limiter;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Cache\RateLimiter  $limiter
+     * @return void
+     */
+    public function __construct(RateLimiter $limiter)
+    {
+        $this->limiter = $limiter;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Tentukan kondisi untuk menonaktifkan rate limiter
+        $disabledIps = [
+            '127.0.0.1',
+            // Tambahkan IP lain jika diperlukan
+        ];
+
+        if (in_array($request->ip(), $disabledIps)) {
+            $this->limiter->clear($this->resolveRequestSignature($request));
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Resolve the request signature.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    protected function resolveRequestSignature(Request $request)
+    {
+        return sha1(
+            $request->method() .
+            '|' . $request->server('SERVER_NAME') .
+            '|' . $request->path() .
+            '|' . $request->ip()
+        );
+    }
+}
